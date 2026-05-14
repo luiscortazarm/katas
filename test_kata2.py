86520c79c43990fa1f024f6eeb1b95fee8739e4f
Luis J Cortazar M <133710389+luiscortazarm@users.noreply.github.com>
2026-05-13 20:44:21 -0700

    Update and rename pytest_kata2.py to test_kata2.py

--- test_kata2.py
+++ test_kata2.py
@@ -5,10 +5,10 @@ def test_calculate_total_cost():
     costs = {'socks': 5.00, 'shoes': 60.00, 'sweater': 30.00}
     tax = 0.09
 
-    # Shopping cart with valid an invalid items
-    cart = ['socks', 'shoes', 'hat']  # 'hat' invalid item
+    # Shopping cart with valid and invalid items
+    cart = ['socks', 'shoes', 'hat']  # 'hat' ignore item
     assert calculate_total_cost(costs, cart, tax) == 70.85
 
-    # Empty shopping carty should return 0.00
+    # Empty shopping cart should return 0.00
     empty_cart = []
     assert calculate_total_cost(costs, empty_cart, tax) == 0.00
